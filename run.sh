e880070e35e30bbaacbcd9a03dfed323bf0e6c5c
Anklebiter <anklebiter87@gmail.com>
2026-05-05 00:32:16 +0000

    updated iptables

--- run.sh
+++ run.sh
@@ -10,17 +10,17 @@ iptables -A INPUT -s 172.16.0.1/12 -j ACCEPT
 iptables -A OUTPUT -d 172.16.0.1/12 -j ACCEPT
 
 # Allow VPN establishment
-iptables -A OUTPUT -p udp -o eth0 -d $VPNIP --dport $VPNPORT -j ACCEPT
-iptables -A INPUT -p udp -i eth0 -s $VPNIP --sport $VPNPORT -j ACCEPT
+#iptables -A OUTPUT -p udp -o eth0 -d $VPNIP --dport $VPNPORT -j ACCEPT
+#iptables -A INPUT -p udp -i eth0 -s $VPNIP --sport $VPNPORT -j ACCEPT
 
 # Allow all TUN connections
 iptables -A OUTPUT -o tun0 -j ACCEPT
 iptables -A INPUT -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # And drop the rest
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+#iptables -P INPUT DROP
+#iptables -P OUTPUT DROP
+#iptables -P FORWARD DROP
 
 cd /etc/openvpn
 
